3034c4b1346770819829438dae13984614b1945b
notwld <mwfarrukh@gmail.com>
2023-05-20 18:19:27 +0500

    -

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
        </div>
        <h2> 🚀 About Me</h2>
       <p className="read-the-docs">
-{/* Hi there👋, my name is Muhammad Waleed and I am a Software Engineering student at Usman Institute of Technology. I am proficient in various programming languages including Python, React, JavaScript, Node/Express, SQL, Prisma, Dotnet, MongoDB, Flask and have experience using platforms such as Heroku and Netlify. I am also skilled in using Git for version control. I am excited to continue learning and growing as a developer and am eager to contribute to projects on GitHub. I aim to deepen my understanding in various areas of technology and product development. */}
+Hi there👋, my name is Muhammad Waleed and I am a Software Engineering student at Usman Institute of Technology. I am proficient in various programming languages including Python, React, JavaScript, Node/Express, SQL, Prisma, Dotnet, MongoDB, Flask and have experience using platforms such as Heroku and Netlify. I am also skilled in using Git for version control. I am excited to continue learning and growing as a developer and am eager to contribute to projects on GitHub. I aim to deepen my understanding in various areas of technology and product development.
       </p>
 
       </div>
